c960bd34298dfa8bacf0c3851508baedc27182ba
René Rafael <rene.rafael@groupsolver.com>
2024-11-01 11:49:18 +0100

    fix(darkstar): Add `shrinkURLNovel` key with `manga` value to hopefully fix 404s

--- src/en/DarkStarTranslations.lua
+++ src/en/DarkStarTranslations.lua
@@ -1,4 +1,4 @@
--- {"id":4418039,"ver":"0.0.2","libVer":"1.0.0","author":"Raf","dep":["Madara>=2.9.1"]}
+-- {"id":4418039,"ver":"0.0.3","libVer":"1.0.0","author":"Raf","dep":["Madara>=2.9.1"]}
 
 local URL = "https://darkstartranslations.com"
 
@@ -11,6 +11,7 @@ return Require("Madara")(URL, {
 	hasCloudFlare = false, -- TODO: It might have cloudflare, I'm not sure yet.
 	latestNovelSel = "div.col-12.col-md-6.badge-pos-1",
 	novelListingURLPath = "manga",
+	shrinkURLNovel = "manga",
 	searchNovelSel = "input.manga-search-field",
 
 	genres = {
