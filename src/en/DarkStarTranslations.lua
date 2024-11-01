6569e4f967c531e9bb5d2f5fa52c2bbc4a843a9b
René Rafael <rene.rafael@groupsolver.com>
2024-11-01 11:43:43 +0100

    fix(darkstar): Update values sent to Madara lib

--- src/en/DarkStarTranslations.lua
+++ src/en/DarkStarTranslations.lua
@@ -1,15 +1,17 @@
--- {"id":4418039,"ver":"0.0.1","libVer":"1.0.0","author":"Raf","dep":["Madara>=2.9.1"]}
+-- {"id":4418039,"ver":"0.0.2","libVer":"1.0.0","author":"Raf","dep":["Madara>=2.9.1"]}
 
 local URL = "https://darkstartranslations.com"
 
 return Require("Madara")(URL, {
 	id = 4418039,
-	name = "DarkStar Translations",
 	imageURL = "",
-	hasCloudFlare = false, -- TODO: It might have cloudflare, I'm not sure yet.
+	name = "DarkStar Translations",
 
-	-- defaults values
-	latestNovelSel = "div.col-6.col-md-3.badge-pos-2",
+	chaptersListSelector = "li.wp-manga-chapter.free-chap",
+	hasCloudFlare = false, -- TODO: It might have cloudflare, I'm not sure yet.
+	latestNovelSel = "div.col-12.col-md-6.badge-pos-1",
+	novelListingURLPath = "manga",
+	searchNovelSel = "input.manga-search-field",
 
 	genres = {
 		"Action",
